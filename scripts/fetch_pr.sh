0c5bc6bfd2948bf711ff256a3e934ea7c45ca36d
Gergely Brautigam <gergely@arangodb.com>
2020-07-18 16:03:51 +0200

    Finished with the fetch pr

--- scripts/fetch_pr.sh
+++ scripts/fetch_pr.sh
@@ -7,6 +7,7 @@ repo="${REPOSITORY}"
 branch="${BRANCH}"
 folder="${FOLDER}"
 docker_token="${DOCKER_TOKEN}"
+docker_username="${DOCKER_USERNAME}"
 
 if [[ -z "${pr}" ]]; then
   echo "PR number is empty. Please set.";
@@ -33,11 +34,16 @@ if [[ -z "${docker_token}" ]]; then
   exit 1
 fi
 
+if [[ -z "${docker_username}" ]]; then
+  echo "DOCKER_USERNAME is empty. Please set.";
+  exit 1
+fi
+
 # Check out PR
 cd "${folder}"
 
 
-# Compile frontend
+# Make release
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 echo 'export NVM_DIR=$HOME/.nvm' >> ~/.bash_profile
 touch $HOME/.nvmrc
@@ -48,5 +54,12 @@ npm cache clean --force
 make download
 make release
 
-# Compile backend
-make compile_backend
+# build docker image
+tag="gaiapipeline/testing:${branch}-${pr}"
+docker build -t "${tag}" ./docker -f ./docker/Dockerfile
+
+# push image to gaia test repo
+echo "${docker_token}" | docker login --username "${docker_username}" --password-stdin
+docker push "${tag}"
+# This output will be used by the gaia-bot to signal flux to deploy this image once it's done pushing.
+echo "${tag}"
