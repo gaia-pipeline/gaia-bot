521423b5f84ad0f5194d408f7d97386270f65a79
Gergely Brautigam <gergely@arangodb.com>
2020-07-20 18:34:09 +0200

    Start docker earlier

--- scripts/fetch_pr.sh
+++ scripts/fetch_pr.sh
@@ -55,6 +55,7 @@ git fetch origin pull/"${pr}"/head:"${branch}"
 git checkout "${branch}"
 
 # Make release
+service docker start
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 echo 'export NVM_DIR=$HOME/.nvm' >> ~/.bash_profile
 touch $HOME/.nvmrc
@@ -66,7 +67,6 @@ make download
 make release
 
 # build docker image
-service docker start
 docker build -t "${tag}" ./docker -f ./docker/Dockerfile
 
 # push image to gaia test repo
